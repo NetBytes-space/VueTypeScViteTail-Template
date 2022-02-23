dc953d09ef003e23a7f712308895edc8823f343b
msterhuj <gabin.lanore@gmail.com>
2022-02-23 22:46:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# VueTypeScViteTail : Vue 3 + Typescript + SCSS + Vite and Tailwindcss
+
+This template should help get you started developing with Vue 3 and Typescript in Vite
+
+More info:
+
+  - [Vue 3](https://vuejs.org/)
+  - [TypeScript](https://www.typescriptlang.org/)
+  - [SCSS](https://sass-lang.com/)
+  - [Vite](https://vite.netlify.com/)
+  - [Tailwindcss](https://tailwindcss.com/)
+
+> Rename project before conding on file package.json line 2
+
+## Install and Running Vite
+
+```bash
+$ npm install
+$ npm run dev
+```
+
+* *Setup by Gabin L from NetBytes*
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const a = ref(0)
+const b = ref(0)
+const total = ref(0)
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline hover:text-sky-400">
+    {{ msg }}
+  </h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Docs
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+  </p>
+
+  <input v-model="a" type="number" />
+  <input v-model="b" type="number" />
+  <button type="button" @click="total = a + b">result is: {{ total }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style scoped></style>
